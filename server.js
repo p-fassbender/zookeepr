df0cc89b792fce94b46bf359f0aab1f42051934a
Preston Fassbender <fassbenderp0551@gmail.com>
2022-02-13 09:30:50 -0600

    removed unneccesary requires

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require("express");
-const { animals } = require("./data/animals");
-const fs = require('fs');
-const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001;
 // http://localhost:3001
 // https://gentle-temple-61041.herokuapp.com
-
+const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
